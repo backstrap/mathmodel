58571d4e0b6417413518c4ea3bf97fb3e1a4f8f2
Kevin Perry <perry@alumni.princeton.edu>
2022-10-02 18:55:43 -0400

    Add geometry option typedefs

--- src/GeometryOptions.js
+++ src/GeometryOptions.js
@@ -1,17 +1,52 @@
-/*
- * Known options:
- *
- * group, color, opacity, colormap, material, colors, renderOrder,
- * complexFunction, singleSide, rotation, translation,
- * fontSize, size, linewidth, useLineSegments, level, fill, maxFaceSlope,
- * openEnded, center, axis, endcaps, radius, steps.
+/**
+ * @typedef {Object} rotationOption
+ * @property {number[]} axis
+ * @property {number} angle
+ * @property {number[]} [origin]
+ */
+/**
+ * @typedef {Object} translationOption
+ * @property {string|function(number):number[]} path
+ * @property {number} [step]
+ */
+/**
+ * @typedef {Object} colorsOption
+ * @property {number} r
+ * @property {number} g
+ * @property {number} b
+ */
+/**
+ * @typedef {Object} geometryOptions
+ * @property {string} [group]
+ * @property {string} [material]
+ * @property {string} [color]
+ * @property {string} [colormap]
+ * @property {colorsOption[]} [colors]
+ * @property {number} [opacity]
+ * @property {number} [renderOrder]
+ * @property {boolean} [complexFunction]
+ * @property {boolean} [singleSide]
+ * @property {rotationOption} [rotation]
+ * @property {translationOption} [translation]
+ * @property {number} [fontSize]
+ * @property {number} [size]
+ * @property {number} [linewidth]
+ * @property {number} [maxFaceSlope]
+ * @property {boolean} [fill]
+ * @property {boolean} [useLineSegments]
+ * @property {boolean} [openEnded]
+ * @property {number[]} [center]
+ * @property {number[]} [axis]
+ * @property {boolean} [endcaps]
+ * @property {number} [radius]
+ * @property {number} [steps]
  */
 /**
  * Class representing mathcell geometry options.
  */
 export class GeometryOptions {
     /**
-     * @param {Object} data - option data as a primitive Object
+     * @param {geometryOptions} data - option data as a primitive Object
      */
     constructor(data = {}) {
         Object.assign(this, data);
